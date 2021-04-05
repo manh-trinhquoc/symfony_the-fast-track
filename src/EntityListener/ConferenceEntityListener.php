c26dfb242b66a75e193a4d503d03eb7a8f9b3d7a
manhtq <manhtq@sunshinegroup.vn>
2021-04-05 17:11:24 +0700

    save point

--- src/EntityListener/ConferenceEntityListener.php
+++ src/EntityListener/ConferenceEntityListener.php
@@ -8,5 +8,18 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 
 class ConferenceEntityListener
 {
-    
+    private $slugger;
+
+    public function __construct(SluggerInterface $slugger)
+    {
+        $this->slugger = $slugger;
+    }
+
+    public function prePersist(Conference $conference, LifecycleEventArgs $event) {
+        $conference->computeSlug($this->slugger);
+    }
+
+    public function preUpdate(Conference $conference, LifecycleEventArgs $event) {
+        $conference->computeSlug($this->slugger);
+    }
 }
